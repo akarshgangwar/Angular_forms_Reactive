0e65a92011e8c8ca0d3eb8ef5774f1b8e391c452
Akarsh Gangwar <akarsh.gangwar@stackpro.in>
2022-08-29 12:41:41 +0530

    basic form completed with formcontrol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import { FormControl , FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loginForm=new FormGroup({
+    name:new FormControl(''),
+    password:new FormControl(''),
+    confirmPassword:new FormControl(''),
+    gender:new FormControl(''),
+    terms:new FormControl('')
+  })
+  constructor(){
+    console.log(this.loginForm.value)
+  }
+
+  onSubmit()
+  {
+    console.log(this.loginForm.value)
+  }
+
   title = 'reactiveForms';
 }
